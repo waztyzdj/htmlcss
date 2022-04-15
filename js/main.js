2bea5d79355790de16e7d09728352eeb01a8c395
wazty_zdj <5999552@qq.com>
2022-04-15 15:20:09 +0800

    1. 首页左侧菜单样式实现。 2. 首页轮播图功能实现。 3. 图片文件目录优化。

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// 轮播图js实现
+
+const maxCount = 4;
+let currCount = 0;
+// 跳转到第n张图片
+function toImage(n) {
+    currCount = n
+    Array.from(document.getElementsByClassName('swiper-item')).forEach((image, index) => index === n ? image.classList.add('curr-item') : image.classList.remove('curr-item'))
+    Array.from(document.getElementsByClassName('dot')).forEach((dot, index) => index === n ? dot.classList.add('curr-dot') : dot.classList.remove('curr-dot'))
+}
+
+// 设置自动播放
+function doOnload() {
+    setInterval(() => {
+        toImage((currCount + 1) % maxCount);
+    }, 3000)
+}
